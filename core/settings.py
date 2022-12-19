b8ee25d6322d8916c44c9c05a2e7faf83f10a65f
eliaschampi <eliaschampi@protonmail.com>
2022-12-18 23:14:07 -0500

    Modelos parte 01

--- core/settings.py
+++ core/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'v1.apps.V2Config',
+    'v1.apps.V1Config',
+    'v2.apps.V2Config',
     'rest_framework'
 ]
 
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = "v1.User"
--- v2/models.py
+++ v2/models.py
@@ -1,6 +1,21 @@
 from django.db import models
+from v1.models import User
 
 class Service(models.Model):
     name = models.CharField(max_length=40)
     description = models.CharField(max_length=300)
     logo = models.CharField(max_length=100)
+
+class PaymentUser(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="pay_users")
+    service = models.ForeignKey(Service, on_delete=models.CASCADE, related_name="ser_users")
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    payment_date = models.DateTimeField(null=False, blank=False)
+    expiration_date = models.DateField(null=False, blank=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+class ExpiredPayment(models.Model):
+    payment_user = models.ForeignKey(PaymentUser, on_delete=models.CASCADE, related_name="expired_pay_users")
+    penalti_fee_amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+ 
+    
