13fa42043024c3d008f892e9c9fae0438fbd4a73
eliaschampi <eliaschampi@protonmail.com>
2022-12-20 23:53:28 -0500

    deploy to render

--- core/settings.py
+++ core/settings.py
@@ -1,10 +1,15 @@
 from pathlib import Path
+import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-_31v_9$w=neaq8)v+$1axt1mjr)8o(&zfwvmo35(j(ebyrkvc_'
+MY_SECRET_KEY = 'django-insecure-_31v_9$w=neaq8)v+$1axt1mjr)8o(&zfwvmo35(j(ebyrkvc_'
 
-DEBUG = True
+SECRET_KEY = os.environ.get('SECRET_KEY', default=MY_SECRET_KEY)
+
+# DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -32,6 +37,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'corsheaders.middleware.CorsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'core.urls'
@@ -56,10 +62,10 @@ WSGI_APPLICATION = 'core.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///db.sqlite3',
+        conn_max_age=600
+    )
 }
 
 CORS_ORIGIN_WHITELIST = (
@@ -67,7 +73,12 @@ CORS_ORIGIN_WHITELIST = (
     'http://localhost:8000',
 )
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
+
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+
+if RENDER_EXTERNAL_HOSTNAME:
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 AUTH_PASSWORD_VALIDATORS = [
     {
@@ -92,7 +103,15 @@ USE_I18N = True
 
 USE_TZ = True
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+
+# Following settings only make sense on production and may break development environments.
+if not DEBUG:    # Tell Django to copy statics to the `staticfiles` directory
+    # in your application directory on Render.
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Turn on WhiteNoise storage backend that takes care of compressing static files
+    # and creating unique names for each version so they can safely be cached forever.
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
