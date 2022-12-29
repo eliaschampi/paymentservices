541c9de0217bd74d0a5b3751bfb72ebbe4489b5a
eliaschampi <eliaschampi@protonmail.com>
2022-12-29 09:05:16 -0500

    addpaymentuser crud

--- v2/api.py
+++ v2/api.py
@@ -2,8 +2,9 @@ from rest_framework import generics, status
 from rest_framework.permissions import IsAuthenticated
 from .permissions import IsAdminUser, CustomPermissionMixin
 from rest_framework.response import Response
-from .models import Service
-from .serializers import ServicesSerializer
+from .models import Service, PaymentUser
+from rest_framework.filters import SearchFilter, OrderingFilter
+from .serializers import ServicesSerializer, PaymentUserSerializer
 from .pagination import MyPagination
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
@@ -29,7 +30,7 @@ class ServiceCreate(CustomPermissionMixin, generics.CreateAPIView):
     permission_classes = (IsAuthenticated, IsAdminUser,)
     authentication_classes = [JWTAuthentication]
 
-    def create(self, request, *args, **kwargs):
+    def create(self, request):
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         self.perform_create(serializer)
@@ -79,3 +80,77 @@ class ServiceDestroy(CustomPermissionMixin, generics.DestroyAPIView):
             'results': []
         }, status=status.HTTP_201_CREATED)
 
+
+class PaymentUserList(generics.ListAPIView):
+    queryset = PaymentUser.objects.all()
+    pagination_class = MyPagination
+    serializer_class = PaymentUserSerializer
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = [JWTAuthentication]
+    filter_backends = (SearchFilter, OrderingFilter)
+    search_fields = ('payment_date', 'expiration_date')
+    ordering = ('-created_at')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['message'] = 'Correctamente listado'
+        context['success'] = True
+        context['code'] = status.HTTP_200_OK
+        return context
+
+
+class PaymentUserCreate(CustomPermissionMixin, generics.CreateAPIView):
+    queryset = PaymentUser.objects.all()
+    serializer_class = PaymentUserSerializer
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = [JWTAuthentication]
+
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        self.perform_create(serializer)
+        headers = self.get_success_headers(serializer.data)
+        return Response({
+            'message': 'Correctamente creado',
+            'success': True,
+            'code': status.HTTP_201_CREATED,
+            'results': serializer.data
+        }, status=status.HTTP_201_CREATED, headers=headers)
+
+
+class PaymentUserUpdate(CustomPermissionMixin, generics.UpdateAPIView):
+    queryset = PaymentUser.objects.all()
+    serializer_class = PaymentUserSerializer
+    permission_classes = (IsAuthenticated, IsAdminUser,)
+    authentication_classes = [JWTAuthentication]
+
+    def update(self, request, pk=None):
+        instance = self.get_object()
+        serializer = self.get_serializer(
+            instance, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        self.perform_update(serializer)
+        return Response({
+            'message': 'Correctamente modificado',
+            'success': True,
+            'code': status.HTTP_200_OK,
+            'results': serializer.data
+        }, status=status.HTTP_201_CREATED)
+
+
+class PaymentUserDestroy(CustomPermissionMixin, generics.DestroyAPIView):
+    queryset = PaymentUser.objects.all()
+    serializer_class = PaymentUserSerializer
+    permission_classes = (IsAuthenticated, IsAdminUser,)
+    authentication_classes = [JWTAuthentication]
+
+    def destroy(self, request, pk=None):
+        instance = self.get_object()
+        print(instance)
+        self.perform_destroy(instance)
+        return Response({
+            'message': 'Correctamente eliminado',
+            'success': True,
+            'code': status.HTTP_200_OK,
+            'results': []
+        }, status=status.HTTP_201_CREATED)
--- v2/urls.py
+++ v2/urls.py
@@ -6,4 +6,8 @@ urlpatterns = [
     path(r'services/create', api.ServiceCreate.as_view(), name='service-create'),
     path(r'services/update/<int:pk>', api.ServiceUpdate.as_view(), name='service-update'),
     path(r'services/destroy/<int:pk>', api.ServiceDestroy.as_view(), name='service-destroy'),
+    path(r'paymentuser/', api.PaymentUserList.as_view(), name='pay-u-list'),
+    path(r'paymentuser/create', api.PaymentUserCreate.as_view(), name='pay-u-create'),
+    path(r'paymentuser/update/<int:pk>', api.PaymentUserUpdate.as_view(), name='pay-u-update'),
+    path(r'paymentuser/destroy/<int:pk>', api.PaymentUserDestroy.as_view(), name='pay-u-destroy'),
 ]
